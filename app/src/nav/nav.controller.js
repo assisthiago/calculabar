2d109a27d62fa60814555e8d9e74a9de5b09fa63
Thiago Assis <thiago.medassis@gmail.com>
2017-05-29 18:14:58 -0300

    Listando/adicionando amigos e pedidos | deletando pedidos | atribuindo compradores ao pedido

--- app/src/nav/nav.controller.js
+++ app/src/nav/nav.controller.js
@@ -10,29 +10,62 @@
         const $ctrl = this;
 
         // vars
-        $ctrl.foo = foo;
+        $ctrl.listFriends = localStorageService.get('Friends') ? localStorageService.get('Friends') : [];
+        // $ctrl.listOrders = localStorageService.get('Orders') ? localStorageService.get('Orders') : [];
+        // $ctrl.listFriends = [];
+        $ctrl.listOrders = [];
 
-        $ctrl.list_friends = [
-            {
-                name: 'Assis Assis',
-                bill: 888.88
-            },
-            {
-                name: 'Assis Thiago',
-                bill: 888.88
-            },
-            {
-                name: 'Thiago Assis',
-                bill: 888.88
-            },
-            {
-                name: 'Thiago Thiago',
-                bill: 888.88
+        // functions
+        $ctrl.addFriend = addFriend;
+        $ctrl.addOrder = addOrder;
+        $ctrl.delOrder = delOrder;
+
+        // public functions
+        function addFriend(name) {
+            if (name) {
+                const friend = {
+                    name: name,
+                    bill: 0.00,
+                    orders: []
+                };
+                $ctrl.friend.name = '';
+
+                $ctrl.listFriends.push(friend);
+                localStorageService.set('Friends', $ctrl.listFriends);
             }
-        ];
+            else return false;
+        };
 
-        function foo(param) {
-            console.log(param);
+        function addOrder(data, arrOfFriends) {
+            if (data && data.name && data.price && arrOfFriends) {
+                const arrWhoPaid = [];
+                angular.forEach(arrOfFriends, (v, k) => {
+                    arrWhoPaid.push(k);
+                });
+
+                const order = {
+                    name: data.name,
+                    price: data.price,
+                    qty: 1,
+                    arrWhoPaid: arrWhoPaid
+                };
+                $ctrl.order.name = '';
+                $ctrl.order.price = '';
+
+                $ctrl.listOrders.push(order);
+                localStorageService.set('Orders', $ctrl.listOrders);
+            }
+            else return false;
+        };
+
+        function delOrder(idx) {
+            $ctrl.listOrders.splice(idx, 1);
+            localStorageService.set('Orders', $ctrl.listOrders);
+        };
+
+        function delFriend(idx) {
+            $ctrl.listFriends.splice(idx, 1);
+            localStorageService.set('Orders', $ctrl.listFriends);
         };
     };
 
